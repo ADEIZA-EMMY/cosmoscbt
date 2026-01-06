630af56249d9d1baf87aa791457eb245ffada960
Admin <adeizaemma47@gmail.com>
2026-01-05 16:02:41 -1100

    Run DB create_all inside app context

--- scripts/fix_db.py
+++ scripts/fix_db.py
@@ -1,12 +1,13 @@
-from code1 import db
+from code1 import app, db
 from sqlalchemy import text
 
-print('DB URI:', db.engine.url)
-conn = db.engine.connect()
-try:
+with app.app_context():
+    print('Ensuring DB tables...')
     db.create_all()
-    res = conn.execute(text("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name"))
-    tables = [r[0] for r in res.fetchall()]
-    print('Tables:', tables)
-finally:
-    conn.close()
+    conn = db.engine.connect()
+    try:
+        res = conn.execute(text("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name"))
+        tables = [r[0] for r in res.fetchall()]
+        print('Tables:', tables)
+    finally:
+        conn.close()
